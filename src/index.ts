e2806ed4215c336b5037657b9cd9415bec636ff6
Rafael Labegalini <rafaellabegalini@vivaworks.com.br>
2024-11-06 17:51:04 -0300

    Autenticação via API externa

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import path from 'path';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Configurar o EJS como motor de visualização
 app.set('view engine', 'ejs');
@@ -12,6 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 // Middleware para receber dados em JSON (ex: AJAX)
 app.use(express.json());
+// Configurar o cookie-parser
+app.use(cookieParser());
 // Middleware para arquivos estáticos (opcional)
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,11 +30,26 @@ app.get('/login', (req, res) => {
 });
 
 // Rota POST para processar o login
-app.post('/login', (req, res) => {
-  const { email, senha } = req.body;
-  // Aqui você pode adicionar a lógica de autenticação
-  // Por enquanto, vamos apenas renderizar uma página de sucesso
-  res.render('login-sucesso', { email });
+app.post('/login', async (req, res) => {
+  const { email, senha: password } = req.body;
+  
+  try {
+    // Enviar requisição para a API NestJS
+    const response = await axios.post('http://localhost:3000/login', {email, password});
+    // Extrair o JWT Token da resposta
+    const { access_token } = response.data;
+    // Salvar o token em um cookie
+    res.cookie('jwt', access_token, {
+      httpOnly: true, // O cookie não pode ser acessado via JavaScript do lado do cliente
+      secure: false,  // Defina como true se estiver usando HTTPS
+      maxAge: 24 * 60 * 60 * 1000, // 1 dia
+    });
+
+    return res.redirect('/');
+  } catch (e) {
+    console.error(e);
+    return res.redirect('/login');
+  }
 });
 
 // Iniciar o servidor
